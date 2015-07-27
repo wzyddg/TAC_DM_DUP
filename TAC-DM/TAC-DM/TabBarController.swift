d24c03f06d6a0532fc001f8feaba0f3f0bd13ba8
anzhehong <anzhehong@126.com>
2015-07-27 09:24:18 +0800

    add frosted sidebar

---
+++ TAC-DM/TAC-DM/TabBarController.swift
@@ -0,0 +1,52 @@
+//
+//  TabBarController.swift
+//  TAC-DM
+//
+//  Created by FOWAFOLO on 15/7/27.
+//  Copyright (c) 2015年 TAC. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    var sidebar: FrostedSidebar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        tabBar.hidden = true
+        
+        moreNavigationController.navigationBar.hidden = true
+        
+        sidebar = FrostedSidebar(itemImages: [
+            UIImage(named: "gear")!,
+            UIImage(named: "globe")!,
+            UIImage(named: "profile")!,
+            UIImage(named: "profile")!,
+            UIImage(named: "profile")!,
+            UIImage(named: "profile")!,
+            UIImage(named: "star")!],
+            colors: [
+                UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
+                UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
+                UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+                UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+                UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+                UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
+                UIColor(red: 119/255, green: 152/255, blue: 255/255, alpha: 1)],
+            selectedItemIndices: NSIndexSet(index: 0))
+        
+        sidebar.isSingleSelect = true
+        sidebar.actionForIndex = [
+            0: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 0}) },
+            1: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 1}) },
+            2: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 2}) },
+            3: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 3}) },
+            4: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 4}) },
+            5: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 5}) },
+            6: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 6}) },
+            7: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 7}) }]
+    }
+    
+}
--- TAC-DM/TAC-DM/ViewController.swift
+++ TAC-DM/TAC-DM/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onBurger() {
+        (tabBarController as! TabBarController).sidebar.showInViewController(self, animated: true)
     }
-
-
 }
 
