42bfd80fd3c4c6d6f2a459e100779b70f2065028
Teng <843018739@qq.com>
2015-08-24 12:03:05 +0800

    create DB completed

--- TAC-DM/TAC-DM/DBModel.swift
+++ TAC-DM/TAC-DM/DBModel.swift
@@ -5,5 +5,11 @@
 //  Created by 一川 on 8/24/15.
 //  Copyright (c) 2015 TAC. All rights reserved.
 //
+//  BorrowInfo Manage functions
 
 import Foundation
+import CoreData
+
+class DBManager {
+    
+}
